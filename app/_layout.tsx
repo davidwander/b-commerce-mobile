c1ffb880cdc67ab966eabce6464316488ba0c254
David <dwander616@gmail.com>
2025-07-26 00:50:42 -0300

    changing navigation model

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,35 @@
 import '../global.css';
 
-import { Stack } from 'expo-router';
+import { Drawer } from 'expo-router/drawer';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'index',
 };
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <Drawer>
+      <Drawer.Screen
+        name="index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: 'Home',
+          title: 'Overview',
+        }}
+      />
+      <Drawer.Screen
+        name="two" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: 'Detalhes',
+          title: 'Detalhes',
+        }}
+      />
+      <Drawer.Screen
+        name="modal"
+        options={{
+          drawerItemStyle: { height: 0 }, // Hide modal from drawer
+        }}
+      />
+    </Drawer>
   );
 }
